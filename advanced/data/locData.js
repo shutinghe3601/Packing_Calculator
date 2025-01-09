3a7d22aebd9e3e5df4be3caff456022792043aff
shutinghe_weee <shuting.he@sayweee.com>
2025-01-09 13:13:14 -0800

    final update

--- advanced/data/locData.js
+++ advanced/data/locData.js
@@ -2,6 +2,6 @@ var locData = [
     {'warehouse_number': 101, 'location_number': 'B03', 'width_inch': 32.0, 'depth_inch': 36, 'height_inch': 20.0, 'loc_inventory': 14,'loc_qty': 2}, 
     {'warehouse_number': 101, 'location_number': 'B07', 'width_inch': 32.0, 'depth_inch': 38.4, 'height_inch': 24.0, 'loc_inventory': 23,'loc_qty': 3}, 
     {'warehouse_number': 101, 'location_number': 'B08', 'width_inch': 40.0, 'depth_inch': 36, 'height_inch': 20.0, 'loc_inventory': 37,'loc_qty': 6}, 
-    {'warehouse_number': 101, 'location_number': 'B18', 'width_inch': 32.0, 'depth_inch': 38.4, 'height_inch': 20.0,  'loc_inventory': 60,'loc_qty': 1}, 
+    {'warehouse_number': 101, 'location_number': 'B18', 'width_inch': 32.0, 'depth_inch': 38.4, 'height_inch': 20.0,  'loc_inventory': 23,'loc_qty': 1}, 
     {'warehouse_number': 101, 'location_number': 'B20', 'width_inch': 40.0, 'depth_inch': 36, 'height_inch': 24.0, 'depth_inch': 36, 'loc_inventory': 14,'loc_qty': 9}
 ];
--- advanced/src/convert_var_csv.js
+++ advanced/src/convert_var_csv.js
@@ -1,20 +1,4 @@
 const fs = require('fs'); 
-// Data array
-var caseData = [
-    {'sku_id': '22', 'name': 'blahblah', 'case_length': 0.0, 'case_width': 0.0, 'case_height': 0.0}, 
-    {'sku_id': '14', 'name': 'blahblah', 'case_length': 11.0, 'case_width': 4.0, 'case_height': 13.0}, 
-    {'sku_id': '23', 'name': 'blahblahblahblah', 'case_length': 10.0, 'case_width': 19.2, 'case_height': 12.6}, 
-    {'sku_id': '37', 'name': 'blahblahblahblah', 'case_length': 11.7, 'case_width': 17.0, 'case_height': 8.5}, 
-    {'sku_id': '60', 'name': 'blahblah', 'case_length': 23.1, 'case_width': 15.8, 'case_height': 4.6}
-];
-
-var locData = [
-    {'warehouse_number': 101, 'location_number': 'B03', 'width_inch': 32.0, 'depth_inch': 36, 'height_inch': 20.0, 'loc_inventory': 14,'case_qty': 2}, 
-    {'warehouse_number': 101, 'location_number': 'B07', 'width_inch': 32.0, 'depth_inch': 38.4, 'height_inch': 24.0, 'loc_inventory': 23,'case_qty': 3}, 
-    {'warehouse_number': 101, 'location_number': 'B08', 'width_inch': 40.0, 'depth_inch': 36, 'height_inch': 20.0, 'loc_inventory': 37,'case_qty': 6}, 
-    {'warehouse_number': 101, 'location_number': 'B18', 'width_inch': 32.0, 'depth_inch': 38.4, 'height_inch': 20.0,  'loc_inventory': 60,'case_qty': 1}, 
-    {'warehouse_number': 101, 'location_number': 'B20', 'width_inch': 40.0, 'depth_inch': 36, 'height_inch': 24.0, 'depth_inch': 36, 'loc_inventory': 14,'case_qty': 9}
-];
 
 // Convert JSON array to CSV
 function jsonToCsv(data) {
